13dcebd0dcb77fc51f26b19f6703306742525d5f
chris <chris@AEGIR>
2024-09-01 18:27:35 +0200

    Projektdateien hinzufügen.

---
+++ ITSMBrowser/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        WV1 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        timRefresh = New Timer(components)
+        CType(WV1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' WV1
+        ' 
+        WV1.AllowExternalDrop = True
+        WV1.CreationProperties = Nothing
+        WV1.DefaultBackgroundColor = Color.White
+        WV1.Location = New Point(66, 48)
+        WV1.Name = "WV1"
+        WV1.Size = New Size(597, 336)
+        WV1.Source = New Uri("https://www.google.de", UriKind.Absolute)
+        WV1.TabIndex = 0
+        WV1.ZoomFactor = 1R
+        ' 
+        ' timRefresh
+        ' 
+        timRefresh.Enabled = True
+        timRefresh.Interval = 60000
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(WV1)
+        Name = "Form1"
+        Text = "ITSM Browser"
+        CType(WV1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents WV1 As Microsoft.Web.WebView2.WinForms.WebView2
+    Friend WithEvents timRefresh As Timer
+
+End Class
---
+++ ITSMBrowser/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class Form1
+    Dim jsonString As String
+    Dim jsonData As Object
+
+    Private Sub WebView21_Click(sender As Object, e As EventArgs) Handles WV1.Click
+
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        WV1.Location = New Point(0, 0)
+
+        Dim NewHeight As Integer = Me.Size.Height - 39
+        Dim NewWidth As Integer = Me.Size.Width - 16
+
+        WV1.Size = New Size(NewWidth, NewHeight)
+    End Sub
+
+    Private Sub timRefresh_Tick(sender As Object, e As EventArgs) Handles timRefresh.Tick
+        WV1.Reload()
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        timRefresh.Stop()
+    End Sub
+
+    Private Sub Form1_Deactivate(sender As Object, e As EventArgs) Handles Me.Deactivate
+        timRefresh.Start()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Try to load the configuration file from different places 
+        If My.Computer.FileSystem.FileExists(AppDomain.CurrentDomain.BaseDirectory + "ITSMBrowser.json") Then
+            jsonString = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "ITSMBrowser.json")
+        ElseIf My.Computer.FileSystem.FileExists(My.Computer.FileSystem.SpecialDirectories.MyDocuments + "\ITSMBrowser.json") Then
+            jsonString = File.ReadAllText(My.Computer.FileSystem.SpecialDirectories.MyDocuments + "\ITSMBrowser.json")
+        End If
+
+        ' Try to convert the file contents to a json object 
+        Try
+            jsonData = JsonConvert.DeserializeObject(Of Object)(jsonString)
+        Catch ex As System.ArgumentNullException
+            Console.WriteLine("No JSON Data in file!")
+        End Try
+
+        ' Read out the configuration and write it to the objects 
+        If jsonData.containsKey("url") Then
+            WV1.Source = New Uri(jsonData("url").ToString())
+        Else
+            Console.WriteLine("URL not present!")
+        End If
+        If jsonData.containsKey("refreshCounter") Then
+            timRefresh.Interval = jsonData("refreshCounter").ToString()
+        Else
+            Console.WriteLine("refreshCounter not present!")
+        End If
+
+    End Sub
+End Class
